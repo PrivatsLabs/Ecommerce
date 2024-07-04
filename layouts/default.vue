55bbdb53f62033f78abccbf30dc065d7ab68da0b
privatslabs <privatslabs@outlook.fr>
2024-07-04 23:56:10 +0000

    search disigner

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,7 @@
 
     <MenuAzVue></MenuAzVue>
     <MenuVue></MenuVue>
+    <SearchVue></SearchVue>
 
     <div class="top-bar shadow">
       <div style="width: 100px;">
@@ -28,7 +29,7 @@
       <div style="display:flex; justify-content: space-between; width: 120px">
        
 
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
+        <svg @click="search" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
 
 
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-bag"><path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z"/><path d="M3 6h18"/><path d="M16 10a4 4 0 0 1-8 0"/></svg>
@@ -121,8 +122,9 @@ export default {
     
     menu(){
      document.getElementById('menu').style.display='block';
-      
-
+    },
+    search(){
+      document.querySelector('.search').style.display='block';
     }
   }
 
