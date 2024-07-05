771e8239c487c7c514e67f81d5d5033c8f8ec918
privatslabs <privatslabs@outlook.fr>
2024-07-05 00:38:26 +0000

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,7 +23,7 @@
             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
           />
         </svg>
-        <h1>LOGO</h1>
+       <nuxt-link to="/"><h1>LOGO</h1></nuxt-link>
       </div>
 
       <div style="display:flex; justify-content: space-between; width: 120px">
@@ -34,8 +34,9 @@
 
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-bag"><path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z"/><path d="M3 6h18"/><path d="M16 10a4 4 0 0 1-8 0"/></svg>
 
-
+        <nuxt-link to='/connexion'>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round"><circle cx="12" cy="8" r="5"/><path d="M20 21a8 8 0 0 0-16 0"/></svg>
+      </nuxt-link>
 
       </div>
     </div>
